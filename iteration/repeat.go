f449214a241c6e18c0933870c0f91d81b7efc179
MH Rilwan <rilwanfit@gmail.com>
2020-03-20 21:09:17 +0100

    Add benchmark

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -2,6 +2,7 @@ package iteration
 
 const repeatedCount = 5
 
+// Repeat repeat a give character 5 times.
 func Repeat(character string) string {
 	var repeated string
 	for i := 0; i < repeatedCount; i++ {
