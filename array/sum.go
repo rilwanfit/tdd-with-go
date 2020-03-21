26fb8bca2ac55c079578d58f31801275efe76797
MH Rilwan <rilwanfit@gmail.com>
2020-03-21 20:00:09 +0100

    The minimal code to make the test pass

--- array/sum.go
+++ array/sum.go
@@ -11,6 +11,13 @@ func Sum(numbers []int) int {
 }
 
 // SumAll which will take a varying number of slices, returning a new slice containing the totals for each slice passed in.
-func SumAll(numbersTosum ...[]int) (sums []int) {
+func SumAll(numbersTosum ...[]int) []int {
+	lengthOfNumbers := len(numbersTosum)
+	sum := make([]int, lengthOfNumbers)
 
+	for i, numbers := range numbersTosum {
+		sum[i] = Sum(numbers)
+	}
+
+	return sum
 }
