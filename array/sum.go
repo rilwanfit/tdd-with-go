3a7230aee9c24dc29584f895fbd2f74307168326
MH Rilwan <rilwanfit@gmail.com>
2020-03-21 18:59:00 +0100

    Test for function Sum which will take an array of numbers wich varying sizes and return the total

--- array/sum.go
+++ array/sum.go
@@ -1,7 +1,7 @@
 package array
 
 // Sum will take an array of numbers and return the total
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
