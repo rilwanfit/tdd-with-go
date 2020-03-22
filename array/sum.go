2d9df2da2bb2bc9369583d1d91361336f10fa09c
MH Rilwan <rilwanfit@gmail.com>
2020-03-22 08:21:13 +0100

    The minimal code to make the test pass

--- array/sum.go
+++ array/sum.go
@@ -26,8 +26,12 @@ func SumAllTails(numbersTosum ...[]int) []int {
 	var sums []int
 
 	for _, numbers := range numbersTosum {
-		tail := numbers[1:]
-		sums = append(sums, Sum(tail))
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
 	return sums
