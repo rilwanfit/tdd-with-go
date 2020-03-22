2310e3847585a4100b121c3354ff524dac1216b3
MH Rilwan <rilwanfit@gmail.com>
2020-03-22 08:40:03 +0100

    The minimal code to make the test pass

--- structs/perimeter.go
+++ structs/perimeter.go
@@ -1,5 +1,6 @@
 package structs
 
+// Perimeter which calculate the perimeter of a rectangle given a height and width.
 func Perimeter(width float64, height float64) float64 {
-	return 0
+	return 2 * (width + height)
 }
