e90e720031725605d4a0339eb68a83efa1e4d691
MH Rilwan <rilwanfit@gmail.com>
2020-03-22 08:43:01 +0100

    Test: Area which returns the area of a rectangle.

--- structs/perimeter.go
+++ structs/perimeter.go
@@ -4,3 +4,8 @@ package structs
 func Perimeter(width float64, height float64) float64 {
 	return 2 * (width + height)
 }
+
+// Area which returns the area of a rectangle.
+func Area(width float64, height float64) float64 {
+	return 0
+}
