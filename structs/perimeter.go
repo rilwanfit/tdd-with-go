529db0f3e2b0868516952caa966344afe69d75c5
MH Rilwan <rilwanfit@gmail.com>
2020-03-22 10:20:41 +0100

    Refactor: add support for new shape

--- structs/perimeter.go
+++ structs/perimeter.go
@@ -25,6 +25,16 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Redius * c.Redius
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+// Area which returns the area of a triangle.
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
 // Perimeter which calculate the perimeter of a rectangle given a height and width.
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
--- structs/perimeter_test.go
+++ structs/perimeter_test.go
@@ -22,12 +22,14 @@ func ExamplePerimeter() {
 }
 
 func TestArea(t *testing.T) {
+	// anonymous struct
 	areaTests := []struct {
 		shape Shape
 		want  float64
 	}{
 		{Rectangle{12.0, 6.0}, 72.0},
 		{Circle{10.0}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 
 	for _, tt := range areaTests {
