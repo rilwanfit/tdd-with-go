c823b49f573481aaf4926a8d4a0627dcb25ca27a
MH Rilwan <rilwanfit@gmail.com>
2020-03-22 08:50:03 +0100

    Refactor: make the code explicit about rectangles

--- structs/perimeter.go
+++ structs/perimeter.go
@@ -1,11 +1,16 @@
 package structs
 
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 // Perimeter which calculate the perimeter of a rectangle given a height and width.
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 // Area which returns the area of a rectangle.
-func Area(width float64, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
